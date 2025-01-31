c4eba7680ced94430290a388d574a0939e72a400
Sai Datta Kunchala <saidatta1432015@gmail.com>
2025-01-31 11:41:15 +0530

    Datta | Add api to send request to ai

--- src/main/java/com/codeworm/ai/spring_ollama_ai/controller/AiController.java
+++ src/main/java/com/codeworm/ai/spring_ollama_ai/controller/AiController.java
@@ -1,9 +1,22 @@
 package com.codeworm.ai.spring_ollama_ai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class AiController {
+    private final ChatClient chatClient;
 
+    public AiController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
+    @GetMapping("/ai")
+    String generation(String userInput){
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
 }
