724d171cf16fe6fa21b2c0fd564bf5b598bd1c04
Sai Datta <saidatta.kunchala@wbd.com>
2025-01-31 11:25:43 +0530

    Datta | initial commit

---
+++ src/main/java/com/codeworm/ai/spring_ollama_ai/SpringOllamaAiApplication.java
@@ -0,0 +1,13 @@
+package com.codeworm.ai.spring_ollama_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOllamaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOllamaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeworm/ai/spring_ollama_ai/SpringOllamaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeworm.ai.spring_ollama_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOllamaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
