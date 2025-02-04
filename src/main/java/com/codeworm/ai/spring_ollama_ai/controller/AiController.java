6d9a87836868aa1105bf4b7612b0a951e0432a84
Sai Datta Kunchala <saidatta1432015@gmail.com>
2025-02-04 10:18:39 +0530

    Datta | Add API to get list of actors

--- src/main/java/com/codeworm/ai/spring_ollama_ai/controller/AiController.java
+++ src/main/java/com/codeworm/ai/spring_ollama_ai/controller/AiController.java
@@ -1,8 +1,12 @@
 package com.codeworm.ai.spring_ollama_ai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/api")
 public class AiController {
@@ -12,11 +16,18 @@ public class AiController {
         this.chatClient = chatClientBuilder.build();
     }
 
+    record Message(String message) {}
+    record ActorFilms(String actor, List<String> movies) {}
+
     @GetMapping("/ai")
-    String generation(String userInput){
-        return this.chatClient.prompt()
-                .user(userInput)
-                .call()
-                .content();
+    Message generation(String userInput){
+        return  this.chatClient.prompt().user(userInput)
+                .call().entity(Message.class);
     }
+
+    @GetMapping("/actors")
+    ActorFilms actors(){
+         return chatClient.prompt().user("Generate the filmography for a random actor").call().entity(ActorFilms.class);
+    }
+
 }
